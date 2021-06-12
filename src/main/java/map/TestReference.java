a71446069b8748e0d9e144dc0b3a3c99d5ce6a83
qincasin <qincasin@163.com>
2021-06-12 12:47:03 +0800

    增加threadlocalrandom 类

---
+++ src/main/java/map/TestReference.java
@@ -0,0 +1,15 @@
+package map;
+
+/**
+ * Created by qincasin on 2021/6/12.
+ */
+public class TestReference {
+    public static void main(String[] args) {
+        Integer tab ;
+        Integer i;
+        int b = 10;
+        tab = i = b;
+        System.out.println(tab+" "+i+" "+ b);
+
+    }
+}
